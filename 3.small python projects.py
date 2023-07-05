e1f859788320a3aa6548ddb6e9095c4d8158a52e
Arpitha Rajeev <108493146+Arpitha-Rajeev1@users.noreply.github.com>
2023-07-05 15:59:14 +0200

    Update 3.small python projects.py

--- 3.small python projects.py
+++ 3.small python projects.py
@@ -23,17 +23,34 @@
 # this creates a server in the localhost:8000
 
 # Project 3: reminder
-import time
-from plyer import notification
+# import time
+# from plyer import notification
 
-while True:
-    notification.notify(
-        title = "Please Drink Water",
-        message = "Else you will be sick",
-        # app_icon = ".ico file"
-        timeout = 5
-)
-    time.sleep(60*60) # reminder every hour
+# while True:
+#     notification.notify(
+#         title = "Please Drink Water",
+#         message = "Else you will be sick",
+#         # app_icon = ".ico file"
+#         timeout = 5
+# )
+#     time.sleep(60*60) # reminder every hour
 
     # to run python in the background: run pythonw .\main.py
 
+# Project 4: Password Generator
+import string
+import random
+
+s1 = string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.digits
+s4 = string.punctuation
+plen = int(input('Enter the password length: '))
+s = []
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+
+random.shuffle(s)
+print("".join(s[0:plen]))
